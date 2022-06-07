7b55e296a60e8c80e092a93f34da4db5696d3834
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-06-07 20:47:54 +0530

    login and register done

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+
+const app = express();
+const PORT = 8080 || process.env.PORT;
+
+dotenv.config();
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use("/api/user", userRouter); // rest APIs, to make code cleaner
+app.use("/api/auth", authRouter);
+
+
+
+app.listen(PORT, () => console.log("server running on port  " + PORT));
