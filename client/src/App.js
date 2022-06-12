a527c14c3b4978316d823aade3326ab2a333c2f0
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-06-12 15:42:23 +0530

    front end

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from '../src/pages/home/Home';
+
+const App = () => {
+    return (
+      <div>
+        <Home/>        
+      </div>
+    );
+}
+export default App;
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import PersonIcon from "@mui/icons-material/Person";
+
+const Home = () => {
+  return (
+    <div>
+      This is home page
+      <Button variant="contained">Home button</Button>
+      <PersonIcon/>
+    </div>
+  );
+};
+
+export default Home;
