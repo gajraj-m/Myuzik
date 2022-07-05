301f438d523352989cda7bf3ff16b19eac595a10
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-07-05 22:43:01 +0530

    iamge left

--- client/src/components/share/Share.js
+++ client/src/components/share/Share.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import "./share.css";
 import PermMediaIcon from "@mui/icons-material/PermMedia";
 import LabelIcon from "@mui/icons-material/Label";
 import RoomIcon from "@mui/icons-material/Room";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import {AuthContext} from "../../context/AuthContext";
+import axios from "axios";
 
 const Share = () => {
+
+  const {user} = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const postDesc = useRef();
+  const [file, setFile] = useState(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id,
+      content: postDesc
+    }
+    try{
+      const res = await axios.post("/post", newPost);
+      res.status(200).json("post sent");
+    } catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <div className="share">
       <div className="shareWrapper">
         <div className="shareTop">
-          <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
+          <img className="shareProfileImg" src={user.profilePic ? PF+user.profilePic : PF+"avatar.png"} alt="" />
           <input
-            placeholder="What's in your mind Safak?"
+            placeholder={"What's in your mind " + user.username + " ?"}
             className="shareInput"
+            ref={postDesc}
           />
         </div>
         <hr className="shareHr" />
-        <div className="shareBottom">
+        <form className="shareBottom" onSubmit={submitHandler}>
           <div className="shareOptions">
-            <div className="shareOption">
+            <label htmlFor="file" className="shareOption">
               <PermMediaIcon htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
-            </div>
+              <input style={{display: "none"}} type="file" id="file" accept="image/*" onChange={(e) => setFile(e.target.files[0])}/>
+            </label>
             <div className="shareOption">
               <LabelIcon htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
@@ -36,8 +60,8 @@ const Share = () => {
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
-          <button className="shareButton">Share</button>
-        </div>
+          <button className="shareButton" type="submit">Share</button>
+        </form>
       </div>
     </div>
   );
